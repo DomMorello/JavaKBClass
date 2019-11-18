7cfe9b2be689eafa654be08a842c3183bbcce4a3
Dom_Morello <zuzudnf@gmail.com>
2019-11-18 17:22:10 +0900

    상속 인삿말 출력 프로그램

--- src/greet/MultiMessage.java
+++ src/greet/MultiMessage.java
@@ -1,97 +1,132 @@
 package greet;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-class MessageEnglish{
-	String[] english = {
-		"0.Hello","1.good morning","2.good night","3.ate?","4.how have u been?"
-		,"5.long time no see","6.nice to meet you","7.see you later"
-	};
+class MessageEnglish extends MultiMessage{
+	String[] english;
 	
-	public void printEnglish() {
-		for(String str : english) {
-			System.out.println(str);
-		}
+	public MessageEnglish() {}
+	
+	public MessageEnglish(String[] english){
+		this.english = english;
 	}
 }
-class MessageJapanese{
-	String[] japanese = {
-			"0.°õ¹æ¿Í","1.¿ÀÇÏÀÌ¿ä","2.Àğ³×","3.½Ä»çÇÏ¼Ìµ¥½º±î?","4.°Û³¢´åÅ¸±î?"
-			,"5.È÷»ç½ÃºÎ¸®","6.µµÁÒ¿À·Î½Ã²ô¿À³×°¡ÀÌ½Ã¸¶½º","7.¶ÇºÁµ¥½º"
-		};
+
+class MessageJapanese extends MultiMessage{
+	String[] japanese;
 	
-	public void printJapanese() {
-		for(String str : japanese) {
-			System.out.println(str);
-		}
+	public MessageJapanese() {}
+	
+	public MessageJapanese(String[] japanese) {
+		this.japanese = japanese;
 	}
 }
-class MessageGerman{
-	String[] german = {
-			"0.±¸ÅÙÅ¹","1.ÁÁÀº¾ÆÄ§ÀÔ´ÏÅ¹!","2.ÀßÀÚ¿äÅ¹","3.¹ä¸Ô¾úÅ¹?","4.ÀßÁö³ÂÅ¹?"
-			,"5.¿À·£¸¸ÀÔ´ÏÅ¹!","6.Ã³À½ºÁÅ¹!","7.¶ÇºÁÅ¹!"
-		};
+
+class MessageGerman extends MultiMessage{
+	String[] german;
 	
-	public void printGerman() {
-		for(String str : german) {
-			System.out.println(str);
-		}
+	public MessageGerman() {}
+	
+	public MessageGerman(String[] german) {
+		this.german = german;
 	}
 }
-class MessageFranch{
-	String[] franch = {
-			"0.ºÀÁê¸£","1.ÁÁÀº¾ÆÄ§Áê¸£","2.ÀßÀÚÁê¸£","3.¹ä¸Ô¾úÁÖ¸£","4.ÀßÁö³»¼ÌÁÖ¸£"
-			,"5.¿À·£¸¸ÀÔÁê¸£","6.Ã³À½ºË°Ú½ÀÁê¸£","7.¶ÇºÁÁê¸£"
-		};
+
+class MessageFranch extends MultiMessage{
+	String[] franch;
 	
-	public void printFranch() {
-		for(String str : franch) {
-			System.out.println(str);
-		}
+	public MessageFranch() {}
+
+	public MessageFranch(String[] french) {
+		this.franch = french;
 	}
 }
-class MessageChinese{
-	String[] chinese = {
-			"0.´ÏÇÏ¿À","1.Á¾Àº¾ÆÄ§ÇÏ¿À","2.Ã°ÀÚÈ¿¿Ë","3.½Ì¼§ÇÏ¼Í½¿´Ï±ø","4.ÀåÂ¡³Ã¼Í½Â´×±ø"
-			,"5.¿Ë·©¸ÁÀ×¿¨¿ë","6.¸Á³¶¼º¹æ°­¿ı¿ë","7.¶Ë¸Á³¶¿ë"
-		};
+
+class MessageChinese extends MultiMessage{
+	String[] chinese;
 	
-	public void printChinese() {
-		for(String str : chinese) {
-			System.out.println(str);
-		}
+	public MessageChinese() {}
+	
+	public MessageChinese(String[] chinese) {
+		this.chinese = chinese;
 	}
 }
 
 public class MultiMessage {
-	MessageEnglish me = new MessageEnglish();
-	MessageJapanese mj = new MessageJapanese();
-	MessageGerman mg = new MessageGerman();
-	MessageFranch mf = new MessageFranch();
-	MessageChinese mc = new MessageChinese();
 	
-	public void getMessage(String input) {
-		if(input.substring(0, 2).equals("en")) {
-			System.out.println(me.english[input.charAt(2)-'0']);
-		}else if(input.substring(0, 2).equals("jp")) {
-			System.out.println(mj.japanese[input.charAt(2)-'0']);
-		}else if(input.substring(0, 2).equals("gm")) {
-			System.out.println(mg.german[input.charAt(2)-'0']);
-		}else if(input.substring(0, 2).equals("fr")) {
-			System.out.println(mf.franch[input.charAt(2)-'0']);
-		}else if(input.substring(0, 2).equals("ch")) {
-			System.out.println(mc.chinese[input.charAt(2)-'0']);
-		}else {
-			System.out.println("ÀÔ·Â¿À·ù!");
+	MessageEnglish me;
+	MessageJapanese mj;
+	MessageGerman mg;
+	MessageFranch mf;
+	MessageChinese mc;
+	
+	public MultiMessage() {}
+
+	public MultiMessage(MessageEnglish me, MessageJapanese mj, MessageGerman mg, MessageFranch mf, MessageChinese mc) {
+		super();
+		this.me = me;
+		this.mj = mj;
+		this.mg = mg;
+		this.mf = mf;
+		this.mc = mc;
+	}
+	
+	public void printAllMessage(String[] arr) {
+		for(String str : arr) {
+			System.out.println(str);
 		}
 	}
 
+	public void getMessage(String input) {
+		input = input.toLowerCase();
+		if (input.length() == 3) {
+			if (input.substring(0, 2).equals("en")) {
+				System.out.println(me.english[input.charAt(2) - '0']);
+			} else if (input.substring(0, 2).equals("jp")) {
+				System.out.println(mj.japanese[input.charAt(2) - '0']);
+			} else if (input.substring(0, 2).equals("gm")) {
+				System.out.println(mg.german[input.charAt(2) - '0']);
+			} else if (input.substring(0, 2).equals("fr")) {
+				System.out.println(mf.franch[input.charAt(2) - '0']);
+			} else if (input.substring(0, 2).equals("ch")) {
+				System.out.println(mc.chinese[input.charAt(2) - '0']);
+			} else {
+				System.out.println("ÀÔ·Â¿À·ù!");
+			}
+		} else {
+			if (input.substring(0, 2).equals("en")) {
+				me.printAllMessage(me.english);
+			} else if (input.substring(0, 2).equals("jp")) {
+				mj.printAllMessage(mj.japanese);
+			} else if (input.substring(0, 2).equals("gm")) {
+				mg.printAllMessage(mg.german);
+			} else if (input.substring(0, 2).equals("fr")) {
+				mf.printAllMessage(mf.franch);
+			} else if (input.substring(0, 2).equals("ch")) {
+				mc.printAllMessage(mc.chinese);
+			} else {
+				System.out.println("ÀÔ·Â¿À·ù!");
+			}
+
+		}
+	}
+	
 	public static void main(String[] args) {
-		
-		MultiMessage mm = new MultiMessage();
+		MessageEnglish me = new MessageEnglish(new String[]{"0.Hello", "1.good morning", "2.good night", 
+				"3.ate?", "4.how have u been?","5.long time no see", "6.nice to meet you", "7.see you later"});
+		MessageJapanese mj = new MessageJapanese(new String[] {"0.°õ¹æ¿Í", "1.¿ÀÇÏÀÌ¿ä", "2.Àğ³×", 
+				"3.½Ä»çÇÏ¼Ìµ¥½º±î?", "4.°Û³¢´åÅ¸±î?", "5.È÷»ç½ÃºÎ¸®", "6.µµÁÒ¿À·Î½Ã²ô¿À³×°¡ÀÌ½Ã¸¶½º", "7.¶ÇºÁµ¥½º"});
+		MessageGerman mg = new MessageGerman(new String[] {"0.±¸ÅÙÅ¹", "1.ÁÁÀº¾ÆÄ§ÀÔ´ÏÅ¹!", "2.ÀßÀÚ¿äÅ¹", 
+				"3.¹ä¸Ô¾úÅ¹?", "4.ÀßÁö³ÂÅ¹?", "5.¿À·£¸¸ÀÔ´ÏÅ¹!", "6.Ã³À½ºÁÅ¹!", "7.¶ÇºÁÅ¹!"});
+		MessageFranch mf = new MessageFranch(new String[] {"0.ºÀÁê¸£", "1.ÁÁÀº¾ÆÄ§Áê¸£", "2.ÀßÀÚÁê¸£", 
+				"3.¹ä¸Ô¾úÁÖ¸£", "4.ÀßÁö³»¼ÌÁÖ¸£", "5.¿À·£¸¸ÀÔÁê¸£", "6.Ã³À½ºË°Ú½ÀÁê¸£", "7.¶ÇºÁÁê¸£"});
+		MessageChinese mc = new MessageChinese(new String[] {"0.´ÏÇÏ¿À", "1.Á¾Àº¾ÆÄ§ÇÏ¿À", "2.Ã°ÀÚÈ¿¿Ë", 
+				"3.½Ì¼§ÇÏ¼Í½¿´Ï±ø", "4.ÀåÂ¡³Ã¼Í½Â´×±ø", "5.¿Ë·©¸ÁÀ×¿¨¿ë", "6.¸Á³¶¼º¹æ°­¿ı¿ë", "7.¶Ë¸Á³¶¿ë"});
+		MultiMessage mm = new MultiMessage(me,mj,mg,mf,mc);
+
 		Scanner sc = new Scanner(System.in);
-		
-		//2-a 
+		// 2-a
 //		while(true) {
 //			System.out.println("* ÀÎ»ç¸» ¼±ÅÃ ÀÔ·Â: ");
 //			String input = sc.next();
@@ -102,18 +137,22 @@ public class MultiMessage {
 //				mm.getMessage(input);
 //			}
 //		}
-		
-		//2-b
-		while(true) {
-			System.out.println("* ÀÎ»ç¸» ¼±ÅÃ ÀÔ·Â: ");
-			String[] input = sc.next().split(",");
-			if(input[0].equals("exit")) {
-				System.out.println("<Á¾·á>");
-				break;
+
+		// 2-b
+		try {
+			while (true) {
+				System.out.println("* ÀÎ»ç¸» ¼±ÅÃ ÀÔ·Â: ");
+				String[] input = sc.next().split(",");
+				if (input[0].equals("exit")) {
+					System.out.println("<Á¾·á>");
+					break;
+				}
+				for (int i = 0; i < input.length; i++) {
+					mm.getMessage(input[i]);
+				}
 			}
-			for(int i=0; i < input.length; i++) {
-				mm.getMessage(input[i]);
-			}
+		} catch (Exception e) {
+			System.out.println("ÀÔ·Â¿À·ù!");
 		}
 	}
 }
